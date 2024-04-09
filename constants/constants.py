a8f097efe0a65fb0812a9cc6d7a888409af1460d
san <deadcove@mail.ru>
2024-04-09 20:11:11 +0300

    Some new color were added.

--- constants/constants.py
+++ constants/constants.py
@@ -12,3 +12,9 @@ class Color(Enum):
     BLACK = (0, 0, 0)
     WHITE = (255, 255, 255)
     YELLOW = (180, 180, 0)
+    ORANGE = (255, 165, 0)
+    PURPLE = (180, 0, 180)
+    FUCHSIA = (153, 50, 168)
+    GRAY = (128, 128, 128)
+    PINK = (255, 192, 203)
+
